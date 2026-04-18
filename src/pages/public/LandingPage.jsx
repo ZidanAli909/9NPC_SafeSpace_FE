b23e11925bc67b98c17ca2ccf0b8466920994f77
Ahmad Zidan Ali <ahmadzidanali909@gmail.com>
2026-04-18 12:40:27 +0700

    feat: initial commit

---
+++ src/pages/public/LandingPage.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/Navbar"
+
+export function LandingPage() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Home</h1>
+        </div>
+    )
+}
