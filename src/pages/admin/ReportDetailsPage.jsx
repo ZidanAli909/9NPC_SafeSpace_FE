7bcd5dc89cae91dfeee2f325041b31f92546b946
jhnarw <jihanauliarw@gmail.com>
2026-05-18 00:34:49 +0700

    feat: update report history,user clicks download report (pdf)

--- src/pages/admin/ReportDetailsPage.jsx
+++ src/pages/admin/ReportDetailsPage.jsx
@@ -102,7 +102,7 @@ function ReportDetailsDetail({
                 </div>
 
                 <div className="mb-4 max-mod:mb-2">
-                    <p className="font-medium text-lg">Detil Kejadian </p>
+                    <p className="font-medium text-lg">Detail Kejadian </p>
                     <div className="text-sm grid grid-cols-4 max-md:grid-cols-2 gap-2 max-md:gap-1 p-4 max-md:p-2">
                         <p className="font-semibold">Jenis Kejadian</p>
                         <p className="font-light">{report?.incident}</p>
@@ -192,7 +192,7 @@ export function ReportDetailsPage() {
                         </BreadcrumbItem>
                         <BreadcrumbSeparator />
                         <BreadcrumbItem>
-                            <BreadcrumbPage className="truncate">Detil Laporan {id}</BreadcrumbPage>
+                            <BreadcrumbPage className="truncate">Detail Laporan {id}</BreadcrumbPage>
                         </BreadcrumbItem>
                     </BreadcrumbList>
                 </Breadcrumb>
--- src/pages/admin/ReportPage.jsx
+++ src/pages/admin/ReportPage.jsx
@@ -59,7 +59,7 @@ function ReportTable({
                         <TableHead className="w-48">Tanggal Laporan</TableHead>
                         <TableHead>Jenis Laporan</TableHead>
                         <TableHead>Status</TableHead>
-                        <TableHead className="w-48">Detil</TableHead>
+                        <TableHead className="w-48">Detail</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
